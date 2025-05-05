b793d8084ae13340b6327ea5f59c1755b2920033
wilson030402 <116888974+wilson030402@users.noreply.github.com>
2025-05-05 15:00:42 +0200

    transposé sans buffer matrice rectangulaire
    transposé sur des matrices rectangulaires

--- transpose_algo.cpp
+++ transpose_algo.cpp
@@ -4,8 +4,8 @@
 
 int main () {
 
-    size_t ligne = 8 ;
-    size_t colonne = 8 ;
+    size_t ligne = 15 ;
+    size_t colonne = 5 ;
     u_int32_t element = ligne * colonne ;
 
     std::queue<int> myFifo ;
@@ -16,28 +16,29 @@ int main () {
     }
 
     int* t1 = new int [element] ; // tableau de sortie
+    
 
-    int* buffer = new int [element/2] ; // buffer intermédiaire 
-
-    //transposé de matrice dans un buffer
-    //while (!myFifo.empty()){
-        for (size_t i = 0 ; i < (ligne/2) ; i++){
+    /*transposé de matrice
+    Choisir de commenter (2) et decommenter (1) pour afficher  la matrice d'origine
+    Choisir de commenter (1) et decommenter (2) pour faire la transposé
+    */
+    while (!myFifo.empty()){
+        for (size_t i = 0 ; i < ligne ; i++){
             for (size_t j = 0 ; j < colonne ; j++){
-                buffer[i*ligne+j] = myFifo.front() ;
+//                t1[i * colonne + j] = myFifo.front() ; // afficher la matrice de départ (1)
+//                std::cout << t1[i * colonne + j] << ' '; // Afficher la matrice de départ(1)
+                
+                t1[j*ligne+i] = myFifo.front() ;         // Transposé (2)
+                
                 myFifo.pop() ;
             }
-        }
-    //}
-
-    for (size_t i = 0; i < ligne/2; ++i) {
-        for (size_t j = 0; j < colonne; ++j) {
-            t1[j * ligne + i] = buffer[i * colonne + j];
+//            std::cout << "\n" ;
         }
     }
+  
 
-
-/*     // lecture de la fifo = lecture du bus Avalon ST
-    while (!myFifo.empty()){
+    // lecture de la fifo = lecture du bus Avalon ST
+/*     while (!myFifo.empty()){
         for (size_t i = 0 ; i < ligne ; i++){
             for (size_t i = 0 ; i < colonne ; i++){
                 std::cout << myFifo.front() << ' ' ;
@@ -45,28 +46,17 @@ int main () {
             }
             std::cout << "\n" ;
         }
-    }
- */
+    } */
 
-//Affiche la le buffer en BRAM
-    for (size_t i = 0 ; i < ligne/2 ; i++){
-        for (size_t j = 0 ; j < colonne ; j++){
-            std::cout << buffer[i*ligne+j] << ' ' ; 
+    // Affichage de la matrice transposé 
+    for (size_t j = 0 ; j < colonne ; j++){
+        for (size_t i = 0 ; i < ligne ; i++){
+            std::cout << t1[j*ligne+i] << ' ' ; 
         }
     std::cout << "\n" ;
-    }
-
- //Affiche tableau finale en DDR 
-    std::cout << "\nMatrice finale:\n " << std::endl;
-    for (size_t i = 0 ; i < ligne ; i++){
-        for (size_t j = 0 ; j < colonne ; j++){
-            std::cout << t1[i*ligne+j] << ' ' ; 
-        }
-    std::cout << "\n" ;
-    }
+}
 
 
     delete[] t1 ;
-    delete[] buffer ;
     return 0 ;
 }
