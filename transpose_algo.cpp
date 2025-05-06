9007534f6ce7dcfb9961d5ab65b66e574f3589c8
wilson030402 <116888974+wilson030402@users.noreply.github.com>
2025-05-06 10:11:16 +0200

    transpose par buffer avec tuile sans boucle

--- transpose_algo.cpp
+++ transpose_algo.cpp
@@ -4,39 +4,242 @@
 
 int main () {
 
-    size_t ligne = 16 ;
-    size_t colonne = 8 ;
+    size_t ligne = 64 ;
+    size_t colonne = 8 ; // ça sera 2048, ça changera pas
     u_int32_t element = ligne * colonne ;
 
-    std::queue<int> myFifo ;
+    //16x8 => 8x16
+    
+    // Je veux faire les itérations 8 par 8 
 
+    //size_t nbPass = ligne / 8;
+
+    size_t nbCols = 8 ;
+    
+    
+    
+    std::queue<int> myFifo ;
     // remplie la fifo qui simule le pipe
     for (u_int32_t i = 0 ; i < element ; i++){
         myFifo.push(i);
     }
-
     int* t1 = new int [element] ; // tableau de sortie
     int* buffer = new int [element] ;
 
-    std::cout << "Buffer de transposition:\n" ;
-    for (size_t i = 0 ; i < ligne/2 ; i++){
-        for (size_t j = 0 ; j < colonne ; j++){
-            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
-            //std::cout << buffer[j*ligne+i] << ' ';  
-            myFifo.pop() ;
+    
+    
+/*     for (int a = 0 ; a < nbPass ; a++ ){
+         std::cout << "Buffer de transposition: "<< nbPass << "\n" ;
+        for (size_t i = 0 ; i < ligne/nbPass ; i++){
+            for (size_t j = 0 ; j < colonne ; j++){
+                buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+                //std::cout << buffer[j*ligne+i] << ' ';  
+                myFifo.pop() ;
         }
         //std::cout << "\n" ; 
+        }
+  
+        for (size_t j = 0 ; j < colonne ; j++){
+            for (size_t i = 0 ; i < ligne/2 ; i++){
+                t1[j*ligne+i] = buffer[j*ligne+i] ;
+                std::cout << buffer[j*ligne+i] << ' ' ; 
+            }
+        std::cout << "\n" ;
+        }
+    } */
+
+
+
+
+    std::cout << "Buffer de transposition:\n" ;
+    for (size_t i = 0 ; i < nbCols; i++){
+        for (size_t j = 0 ; j < colonne ; j++){
+            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+            std::cout << buffer[j*ligne+i] << ' ';  
+            myFifo.pop() ;
+        }
+        std::cout << "\n" ; 
     }
   
+    std::cout << "hello" << std::endl;
     for (size_t j = 0 ; j < colonne ; j++){
         for (size_t i = 0 ; i < ligne/2 ; i++){
             t1[j*ligne+i] = buffer[j*ligne+i] ;
-            std::cout << buffer[j*ligne+i] << ' ' ; 
         }
-    std::cout << "\n" ;
     }
 
     std::cout << "\n\n" ;
+
+    std::cout << "Buffer de transposition 2 :\n" ;
+    for (size_t i = 0 ; i < nbCols; i++){
+        for (size_t j = 0 ; j < colonne ; j++){
+            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+            std::cout << buffer[j*ligne+i] << ' ';  
+            myFifo.pop() ;
+        }
+        std::cout << "\n" ; 
+    }
+
+    std::cout << "\n\n" ;
+
+    size_t passite = 2 ;
+    for (size_t j = 0 ; j < colonne ; j++){
+        for (size_t i = 0 ; i < nbCols ; i++){
+            t1[j*ligne+i + (nbCols * (passite - 1))] = buffer[j*ligne+i] ;
+            //std::cout << buffer[j*ligne+i] << ' ' ; 
+        }
+    //std::cout << "\n" ;
+    }
+
+    std::cout << "Buffer de transposition 3 :\n" ;
+    for (size_t i = 0 ; i < nbCols; i++){
+        for (size_t j = 0 ; j < colonne ; j++){
+            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+            std::cout << buffer[j*ligne+i] << ' ';  
+            myFifo.pop() ;
+        }
+        std::cout << "\n" ; 
+    }
+
+    std::cout << "\n\n" ;
+
+    passite = 3 ;
+    for (size_t j = 0 ; j < colonne ; j++){
+        for (size_t i = 0 ; i < nbCols ; i++){
+            t1[j*ligne+i + (nbCols * (passite - 1))] = buffer[j*ligne+i] ;
+        }
+    }
+
+    std::cout << "Buffer de transposition 4 :\n" ;
+    for (size_t i = 0 ; i < nbCols; i++){
+        for (size_t j = 0 ; j < colonne ; j++){
+            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+            std::cout << buffer[j*ligne+i] << ' ';  
+            myFifo.pop() ;
+        }
+        std::cout << "\n" ; 
+    }
+
+    std::cout << "\n\n" ;
+
+    passite = 4 ;
+    for (size_t j = 0 ; j < colonne ; j++){
+        for (size_t i = 0 ; i < nbCols ; i++){
+            t1[j*ligne+i + (nbCols * (passite - 1))] = buffer[j*ligne+i] ;
+        }
+    }
+
+    std::cout << "Buffer de transposition 5 :\n" ;
+    for (size_t i = 0 ; i < nbCols; i++){
+        for (size_t j = 0 ; j < colonne ; j++){
+            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+            std::cout << buffer[j*ligne+i] << ' ';  
+            myFifo.pop() ;
+        }
+        std::cout << "\n" ; 
+    }
+
+    std::cout << "\n\n" ;
+
+    passite = 5 ;
+    for (size_t j = 0 ; j < colonne ; j++){
+        for (size_t i = 0 ; i < nbCols ; i++){
+            t1[j*ligne+i + (nbCols * (passite - 1))] = buffer[j*ligne+i] ;
+        }
+    }
+
+    std::cout << "Buffer de transposition 6 :\n" ;
+    for (size_t i = 0 ; i < nbCols; i++){
+        for (size_t j = 0 ; j < colonne ; j++){
+            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+            std::cout << buffer[j*ligne+i] << ' ';  
+            myFifo.pop() ;
+        }
+        std::cout << "\n" ; 
+    }
+
+    std::cout << "\n\n" ;
+
+    passite = 6 ;
+    for (size_t j = 0 ; j < colonne ; j++){
+        for (size_t i = 0 ; i < nbCols ; i++){
+            t1[j*ligne+i + (nbCols * (passite - 1))] = buffer[j*ligne+i] ;
+        }
+    }
+
+    std::cout << "Buffer de transposition 7 :\n" ;
+    for (size_t i = 0 ; i < nbCols; i++){
+        for (size_t j = 0 ; j < colonne ; j++){
+            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+            std::cout << buffer[j*ligne+i] << ' ';  
+            myFifo.pop() ;
+        }
+        std::cout << "\n" ; 
+    }
+
+    std::cout << "\n\n" ;
+
+    passite = 7 ;
+    for (size_t j = 0 ; j < colonne ; j++){
+        for (size_t i = 0 ; i < nbCols ; i++){
+            t1[j*ligne+i + (nbCols * (passite - 1))] = buffer[j*ligne+i] ;
+        }
+    }
+
+    std::cout << "Buffer de transposition 8 :\n" ;
+    for (size_t i = 0 ; i < nbCols; i++){
+        for (size_t j = 0 ; j < colonne ; j++){
+            buffer[j*ligne+i] = myFifo.front() ;         // Transposé (2)   
+            std::cout << buffer[j*ligne+i] << ' ';  
+            myFifo.pop() ;
+        }
+        std::cout << "\n" ; 
+    }
+
+    std::cout << "\n\n" ;
+
+    passite = 8 ;
+    for (size_t j = 0 ; j < colonne ; j++){
+        for (size_t i = 0 ; i < nbCols ; i++){
+            t1[j*ligne+i + (nbCols * (passite - 1))] = buffer[j*ligne+i] ;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
 /*      //transposé de matrice
     //Choisir de commenter (2) et decommenter (1) pour afficher  la matrice d'origine
@@ -68,7 +271,7 @@ int main () {
         }
     } */
 
-    // Affichage de la matrice transposé 
+     // Affichage de la matrice transposé 
      for (size_t j = 0 ; j < colonne ; j++){
         for (size_t i = 0 ; i < ligne ; i++){
             std::cout << t1[j*ligne+i] << ' ' ; 
