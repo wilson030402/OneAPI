990e80b877bcb9065c27a276cd46c70e52d0dd59
wilson030402 <116888974+wilson030402@users.noreply.github.com>
2025-05-14 15:07:23 +0200

    double buffering 99% ok

--- transpose_finale.cpp
+++ transpose_finale.cpp
@@ -14,19 +14,17 @@ using pipe_props = decltype(
     sycl::ext::oneapi::experimental::properties(
         sycl::ext::intel::experimental::ready_latency<0>));
 
-// On passe maintenant à un pipe de sycl::vec<float,2>
-using Complex   = sycl::vec<float,2>;
-using Cplx   = ac_complex<float>;
+using Complex   = ac_complex<float>;
 
 using InputPipe = sycl::ext::intel::experimental::pipe<
-    IdPipeA, Cplx, 0, pipe_props>;
+    IdPipeA, Complex, 0, pipe_props>;
 
 // Propriétés pour l'annotation de l'output
 using out_props = decltype(
     sycl::ext::oneapi::experimental::properties{
         sycl::ext::intel::experimental::buffer_location<Kbl1>,
         sycl::ext::intel::experimental::dwidth<512>, //512
-        sycl::ext::intel::experimental::maxburst<4>, // 4
+        sycl::ext::intel::experimental::maxburst<8>, // 4
         sycl::ext::intel::experimental::latency<0>,
         sycl::ext::intel::experimental::read_write_mode_write,
         // un Complex fait 2×32 bits = 64 bits = 8 octets d'alignement
@@ -39,7 +37,7 @@ using LSU512 = sycl::ext::intel::lsu<
 struct Transpose {
   // out pointe maintenant sur un tableau de Complex
   sycl::ext::oneapi::experimental::annotated_arg<
-  Cplx*,
+  Complex*,
       out_props>
       out;
   sycl::ext::oneapi::experimental::annotated_arg<
@@ -53,10 +51,11 @@ struct Transpose {
           sycl::ext::intel::experimental::conduit})>
       cols; // colonne
 
+    [[intel::kernel_args_restrict]]
     void operator()() const {
 
       // pragma unroll sur l'écriture doit avoir le même N que N réplication du buffer
-     [[intel::numbanks(8),intel::bankwidth(64),intel::max_replicates(1)]] Cplx buffer[2048][32];
+     [[intel::numbanks(16),intel::bankwidth(64),intel::max_replicates(1)]] Complex buffer[2][2048][32];
 
     [[intel::fpga_register]]   size_t ligne = rows ;
     [[intel::fpga_register]] size_t colonne = cols ; // ça sera 2048 au max , ça changera pas
@@ -64,21 +63,22 @@ struct Transpose {
     [[intel::fpga_register]] size_t nbCols = 32 ;   // Je veux faire les itérations 32 par 32 
     [[intel::fpga_register]] size_t nbPass = ligne / nbCols;
 
+    [[intel::initiation_interval(1),intel::ivdep(buffer)]]
     for (size_t a = 0 ; a < nbPass ; a++ ){
       [[intel::loop_coalesce(2)]]
       for (size_t i = 0 ; i < nbCols; i++){
          for (size_t j = 0 ; j < colonne ; j++){
-             buffer[j][i] = InputPipe::read()  ;         // Transposé (2)    
+             buffer[a%2][j][i] = InputPipe::read()  ;         // Transposé (2)    
          }
      }
 
-     [[intel::loop_coalesce(2),intel::ivdep]]
+     [[intel::loop_coalesce(2)]]
      for (size_t j = 0 ; j < colonne ; j++){
-        #pragma unroll (8)
+        #pragma unroll
          for (size_t i = 0 ; i < nbCols ; i++){
              LSU512::store( sycl::address_space_cast<
                             sycl::access::address_space::global_space,
-                            sycl::access::decorated::no>(out + j*ligne+i + (nbCols *a)), buffer[j][i]);
+                            sycl::access::decorated::no>(out + j*ligne+i + (nbCols *a)), buffer[a%2][j][i]);
          }
       } 
     }  
@@ -100,12 +100,12 @@ int main() {
               << q.get_device().get_info<sycl::info::device::name>()
               << '\n';
 
-    const uint32_t rows     = 64;
-    const uint32_t cols     = 64;
+    const uint32_t rows     = 128;
+    const uint32_t cols     = 128;
     const size_t   elements = size_t(rows) * cols;
 
     // Allocation d'un tableau de Complex
-    Cplx* b = sycl::malloc_shared<Cplx>(
+    Complex* b = sycl::malloc_shared<Complex>(
       elements, q,
       {sycl::ext::intel::experimental::property::usm::buffer_location(Kbl1)}) ;
 
@@ -113,7 +113,7 @@ int main() {
     for (uint32_t r = 0; r < rows; ++r) {
       for (uint32_t c = 0; c < cols; ++c) {
         // On simule un complexe (re, im = .5)
-        Cplx a_vec(float(r * cols + c),
+        Complex a_vec(float(r * cols + c),
                       float(r * cols + c) + 0.5f);
          //std::cout << '(' << a_vec[0] << ',' << a_vec[1] << ") ";
         InputPipe::write(q, a_vec);
@@ -131,7 +131,7 @@ int main() {
     bool ok = true;
     for (uint32_t r = 0; r < cols; ++r) {
       for (uint32_t c = 0; c < rows; ++c) {
-        Cplx v = b[r * rows + c];
+        Complex v = b[r * rows + c];
          //std::cout << '(' << v[0] << ',' << v[1] << ") " ;//<< &b[r * rows + c]<<' ';
         // On vérifie que la partie réelle est c*cols + r
         // et que l'imaginaire est +0.5
